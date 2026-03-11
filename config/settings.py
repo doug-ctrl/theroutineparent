1c353c86e010f7d09e1013e2ca44bf343f741514
Douglas Tieni <douglasshikutwa@gmail.com>
2026-03-11 17:27:04 +0000

    feat: add whitenoise, python-dotenv, and deployment settings

--- config/settings.py
+++ config/settings.py
@@ -11,21 +11,23 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(os.path.join(BASE_DIR, ".env"))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7gyplkfrli7xrh4+37rrr9(21j$ii6t*0@d64+=of2!-5vn3dp'
+SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", "False") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
@@ -46,6 +48,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -126,8 +129,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-import os
-
 # Templates
 TEMPLATES[0]["DIRS"] = [os.path.join(BASE_DIR, "templates")]
 
@@ -141,3 +142,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 # Site info
 SITE_NAME = "ModernMuse"
 SITE_TAGLINE = "Productivity for Modern Parents"
+
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
